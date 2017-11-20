ffc766849d19d37904a53cb7758ad1d1385308aa
dhiren <kashiwale@yahoo.com>
2017-11-19 22:42:11 -0500

    changed database to postgre

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hello',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
